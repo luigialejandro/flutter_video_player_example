0b19451fa66280b32762e44f3ca39065f425ac5c
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-07-14 21:28:50 -0500

    Preparing the project and adding dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Video player demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
